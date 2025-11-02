96c6ec02b84f1ce2f03733fc12ea28e28eb9f6a1
Govind-ICL <nrgovind@iclsoftwares.in>
2025-11-02 08:04:00 +0000

    testing commit 2

--- postadmission.php
+++ postadmission.php
@@ -196,6 +196,7 @@ if (isset($_POST['submit'])) {
         mysqli_commit($conn);
     
     } catch (Exception $e) {
+        echo "<script>alert($e);</script>";
         // ❌ Rollback on error
         mysqli_rollback($conn);
     
